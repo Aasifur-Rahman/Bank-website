2f4ddd044ddb6bc710324ffaed55739e75729420
Siam Rahman <asifur.web@gmail.com>
2023-06-04 00:32:39 +0600

    navbar is completed now some favicons to add

--- js/bank.js
+++ js/bank.js
@@ -1,6 +1,8 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const menu = document.querySelector(".menu");
+const logButton = document.querySelector(".btn");
 
 menuToggle.addEventListener("click", function () {
   menu.classList.toggle("show");
+  logButton.classList.toggle("logout");
 });
