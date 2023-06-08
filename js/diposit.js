33caa0d63d12794f3f9b83bd5c0e807c939e4ead
Siam Rahman <asifur.web@gmail.com>
2023-06-08 07:23:46 +0600

    another logic completed

--- js/diposit.js
+++ js/diposit.js
@@ -8,8 +8,14 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   const previousDepositTotal = parseFloat(previousDepositTotalString);
 
   const currentDepositTotal = previousDepositTotal + newDepositAmount;
-
   depositTotalElement.innerText = currentDepositTotal;
+
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+  balanceTotalElement.innerText = currentBalanceTotal;
 
   depositField.value = " ";
 });
